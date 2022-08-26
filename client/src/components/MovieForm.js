68a541f1fa1cd189e5c1dc7058f17eaa2f036a7f
youbaidjis <youba.etud@gmail.com>
2022-08-26 11:11:35 -0700

    Completed assignment

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([]);
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -14,17 +15,23 @@ function MovieForm() {
     female_director: false,
   });
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
-    fetch("/movies", {
+    // fetch returns a Promise, we must await it
+    const response = await fetch("/movies", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+    });
+    // response.json() returns a Promise, we must await it
+    const data = await response.json();
+    if (response.ok) {
+      console.log("Movie created:", data);
+    } else {
+      setErrors(data.errors);
+    }
   }
 
   function handleChange(e) {
@@ -125,6 +132,15 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+        {
+  errors.length > 0 && (
+    <ul style={{ color: "red" }}>
+      {errors.map((error) => (
+        <li key={error}>{error}</li>
+      ))}
+    </ul>
+  )
+}
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
